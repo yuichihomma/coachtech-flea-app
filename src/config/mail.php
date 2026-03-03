7606ca62ac5f9f5e2239cbae505cb656e20ea734
yuichi homma <zm16178@gmail.com>
2026-03-03 11:55:26 +0900

    Update README: add dummy users and login credentials

--- src/config/mail.php
+++ src/config/mail.php
@@ -88,10 +88,6 @@ return [
 
         'postmark' => [
             'transport' => 'postmark',
-            // 'message_stream_id' => env('POSTMARK_MESSAGE_STREAM_ID'),
-            // 'client' => [
-            //     'timeout' => 5,
-            // ],
         ],
 
         'resend' => [
